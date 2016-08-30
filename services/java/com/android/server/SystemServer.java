eee5b9285a2b9ffc1b439d058f929221c799a379
darkobas <darkobas@gmail.com>
2016-08-30 18:47:14 +0200

    theme: Revert "Remove DayNight theme"
    Squashed:
    
        -Revert "DO NOT MERGE: Remove DayNight theme"
            d9aab73a3879dcf61b011e098bd43ab35e478c56
        -Dark theme: fix downloads background not being seen when night theme selected
    thanks to Brandon M for pointing out the fix
            4d1192d20f7e8b439b90d803574710a5a1ab7173
        -base: adapt for dark theme
            bbe7349c1061219528466d8f84621230e1a91806
    
    Change-Id: I877aa1db7eeeacc09441b94ac9b10e28e742c48a

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -199,7 +199,7 @@ public final class SystemServer {
      * visual content.
      */
     private static final int DEFAULT_SYSTEM_THEME =
-            com.android.internal.R.style.Theme_DeviceDefault_System;
+            com.android.internal.R.style.Theme_Material_DayNight_DarkActionBar;
 
     private final int mFactoryTestMode;
     private Timer mProfilerSnapshotTimer;
@@ -426,7 +426,7 @@ public final class SystemServer {
     private void createSystemContext() {
         ActivityThread activityThread = ActivityThread.systemMain();
         mSystemContext = activityThread.getSystemContext();
-        mSystemContext.setTheme(DEFAULT_SYSTEM_THEME);
+        mSystemContext.setTheme(android.R.style.Theme_Material_DayNight_DarkActionBar);
     }
 
     /**
