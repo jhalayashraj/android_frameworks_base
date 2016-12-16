7800ecf5e987d376215962e084eb89f83809dc27
Yashraj Jhala <jhalayashraj@gmail.com>
2016-12-17 00:38:59 +0530

    import Settings provider

--- core/java/android/widget/Toast.java
+++ core/java/android/widget/Toast.java
@@ -31,6 +31,7 @@ import android.os.IBinder;
 import android.os.Message;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
